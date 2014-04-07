274613a3476051b2f050eb6c4310dd4548639507
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-04-07 11:27:55 -0700

    added secret key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Locallyv2::Application.config.secret_key_base = 'b3025337a505c4ee8eeb1d3ef5e590b767b99809d8e05f1b3c256dc6051ae4de28b166206240d39ba64e257d6120a0adb573363b59712dbbca0326414b97f4cc'
+Locallyv2::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
