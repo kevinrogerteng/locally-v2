4b615e1e7d32ad98a72b6d3d38a82162d1c5f4e5
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-04-09 15:44:15 -0700

    made some adjustments to parameters for trip requests specs

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,12 +2,28 @@ class TripsController < ApplicationController
 
   def index
 
-    trips = Trip.where(user_id: params[:id])
+    trips = Trip.where(user_id: params[:current_user])
+    user = User.find(params[:current_user])
 
     respond_to do |f|
         f.html {render :layout => false}
-        f.json {render :json => trips.as_json(:include => [:user])}
+        f.json {render :json => {
+          :trips => trips,
+          :user => user
+          }
+        }
         #f.json {render :json => @posts.as_json(:include=> [:tickets, :user])}
+    end
+  end
+
+  def show
+
+    trip = Trip.find(params[:id])
+
+    respond_to do |f|
+      f.html {render :layout => false}
+      f.json {render :json => trip.as_json(:include => [:activities])}
+      #f.json {render :json => @posts.as_json(:include=> [:tickets, :user])}
     end
 
   end
--- spec/requests/trip_spec.rb
+++ spec/requests/trip_spec.rb
@@ -7,27 +7,60 @@ describe "Trip" do
     @trip = create(:trip, user_id: @user.id)
   end
 
-  describe "GET JSON with /trips.json ON index method"
+  describe "GET JSON with /trips.json ON index method" do
 
-    it "should be successful" do
-      get trips_path
+    it "should be successful for HTML" do
+      id = {:current_user => @user.id}
+      get trips_path(id)
       response.status.should == 200
+    end
+
+    it "should be successful for JSON" do
+      json = { :format => 'json'}
+      id = {:current_user => @user.id}
+      get trips_path(id), json
+      response.status.should == 200
+
     end
 
     it "should respond as JSON" do
       json = { :format => 'json'}
-      get trips_path, json
+      id = {:current_user => @user.id}
+      get trips_path(id), json
       result = JSON.parse(response.body)
-      result.should be_kind_of Array
+      result.should be_kind_of Hash
       result.length.should_not be_nil
     end
 
     it "should return trip(s) that belongs to the signed in user" do
       json = { :format => 'json'}
-      id = {:id => @user.id}
+      id = {:current_user => @user.id}
       get trips_path(id), json
       result = JSON.parse(response.body)
-      result.first['user']['email'].should eq(@user.email)
+      result['user']['email'].should eq(@user.email)
     end
+
+  end
+
+  describe "GET JSON with /trips/:id.json ON show method" do
+
+    it 'should be successful' do
+      get trip_path(@trip)
+      response.status.should == 200
+    end
+
+    it 'should respond as JSON' do
+      json = { :format => 'json'}
+      get trip_path(@trip), json
+      result = JSON.parse(response.body)
+      result.should be_kind_of Hash
+      result.length.should_not be_nil
+      response.status.should == 200
+    end
+
+    it 'should return the selected trip'
+    it 'should return activities'
+
+  end
 
 end
