32d077b4c273ecbe99b400371a9f3a681fe7442a
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-04-11 11:59:21 -0700

    refactored user specs

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -27,14 +27,16 @@ describe "User" do
   end
 
   describe "POST JSON with /users.json on create method" do
+
+    before :each do
+      post users_path user: {email: "sample@example.com", screen_name: "pengusan" ,password: "password", password_confirmation: "password"}
+    end
     
     it 'should be successful' do
-      post users_path user: {email: "sample@example.com", screen_name: "pengusan" ,password: "password", password_confirmation: "password"}
       response.status.should == 200
     end
 
     it 'should create a new user' do
-      post users_path user: {email: "sample@example.com", screen_name: "pengusan" ,password: "password", password_confirmation: "password"}
       result = JSON.parse(response.body)
       result['success'].should eq("Welcome!")
 
@@ -49,28 +51,22 @@ describe "User" do
       before (:each) do 
         @user = create(:user, email: "sample@example.com", screen_name: "pengusan" ,password: "password", password_confirmation: "password")
         @params = {}
-      end
-
-      it 'should be successful' do
         @params[:hometown] = "San Francisco"
         @params[:screen_name] = "pandasan"
         patch user_path @user.id, :user => @params.as_json
+      end
+
+      it 'should be successful' do
         @user.reload
         response.status.should == 200
       end
 
       it 'should return a success message' do
-        @params[:hometown] = "San Francisco"
-        @params[:screen_name] = "pandasan"
-        patch user_path @user.id, :user => @params.as_json
         result = JSON.parse(response.body)
         result['success'].should eq("Account Updated")
       end
 
       it 'should persist the updates' do
-        @params[:hometown] = "San Francisco"
-        @params[:screen_name] = "pandasan"
-        patch user_path @user.id, :user => @params.as_json
         result = JSON.parse(response.body)
         result["updates"]["hometown"].should eq("San Francisco")
       end
@@ -82,17 +78,15 @@ describe "User" do
 
     before (:each) do 
         @user = create(:user, email: "sample@example.com", screen_name: "pengusan" ,password: "password", password_confirmation: "password")
+        json = { :format => 'json'}
+        delete user_path @user, json
     end
 
     it 'should be successful' do
-      json = { :format => 'json'}
-      delete user_path @user, json
       response.status.should == 200
     end
 
     it 'should return a success message' do
-      json = { :format => 'json'}
-      delete user_path @user, json
       result = JSON.parse(response.body)
       result['success'].should eq("Account deleted")
     end
