61ca2e1cbc98f3837ceceb0a781aec985629ee7a
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-04-15 10:59:52 -0700

    added more seeds for activities and added templates

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,12 @@ trips = []
 
 trips << Trip.create(name: 'SF Galore', destination: "San Francisco, California", description:"moving to SF!!",)
 trips << Trip.create(name: 'New York Fun!', destination: "Manhattan, New York", description:"Visiting Sasha!")
+trips << Trip.create(name: 'Port Port!',  destination: "Portland, Oregon", description:"Saying hi to Granny!")
+trips << Trip.create(name: 'BBQ IT!', destination: "Dallas, Texas", description:"Let's be a cowboy!")
+trips << Trip.create(name: 'Comic-con!', destination: "San Diego, California", description:"I'm going as a Star member from Resident Evil!")
 
 User.first.trips << trips[0]
 User.first.trips << trips[1]
+User.first.trips << trips[2]
+User.first.trips << trips[3]
+User.first.trips << trips[4]
